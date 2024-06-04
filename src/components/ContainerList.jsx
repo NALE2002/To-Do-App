450503f5cc04ef5cb83abefc55a72f694415d994
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-04 23:22:46 +0200

    ulteriori cambiamenti

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -1,12 +1,14 @@
 
 
 function ContainerList(){
-    return <ul className="ul-main">
-                <li className="li-main">My projects</li>
+    return  <><h2>Your Projects</h2>
+            <ul className="ul-main">
+                <li className="li-main">project 0</li>
                 <li className="li-main">project 1</li>
                 <li className="li-main">project 2</li>
                 <li className="li-main">project 3</li>
             </ul>
+            </>
 }
 
 export default ContainerList;
--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -2,7 +2,7 @@ import ContainerList from "./ContainerList";
 
 function AsideBar(){
     return <aside className="aside">
-                <h1>To-Do-WebApp</h1>
+                <h1>To-Do WebApp</h1>
                 <ContainerList/>
             </aside>
 }
