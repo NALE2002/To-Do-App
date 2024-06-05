8399076d38a2c098c80bea6a6fdb6aa1c56441c3
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-06 01:30:16 +0200

    sistemata navbar e altri bug

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -4,12 +4,7 @@ function ContainerList(){
     return  (
         <>
             <h2>Your Projects</h2>
-            <ul className="ul-main">
-                <li className="li-main">project 0</li>
-                <li className="li-main">project 1</li>
-                <li className="li-main">project 2</li>
-                <li className="li-main">project 3</li>
-            </ul>
+            
         </>
     )
 }
