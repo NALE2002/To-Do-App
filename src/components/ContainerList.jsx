6a40aebb6f33f9db86b3fba8fab329f020b493d0
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-10 23:09:09 +0200

    rimossa grid e aggiustata sidenav generale

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -6,7 +6,13 @@ function ContainerList(){
         <div className="sub-title">  
             <h3>Your Projects: </h3>
         </div>
-        
+        <div className="proj-content">
+            <ul className="proj-ul">
+                <li className="proj-li"><p>To-Do WebApp</p></li>
+                <li className="proj-li"><p>TypeGame</p></li>
+                <li className="proj-li"><p>Domani devo andare in palestra</p></li>
+            </ul>
+        </div>
         </>
     )
 }
