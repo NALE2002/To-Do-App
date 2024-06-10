8528ef2937bb86333ed005192e22f156a322a13d
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-11 00:33:55 +0200

    aggiunti ulteriori cambiamenti a sidenav

--- src/components/ContainerList.jsx
+++ src/components/ContainerList.jsx
@@ -10,7 +10,7 @@ function ContainerList(){
             <ul className="proj-ul">
                 <li className="proj-li"><p>To-Do WebApp</p></li>
                 <li className="proj-li"><p>TypeGame</p></li>
-                <li className="proj-li"><p>Domani devo andare in palestra</p></li>
+                {/* <li className="proj-li"><p>Domani devo andare in palestra</p></li> */}
             </ul>
         </div>
         </>
