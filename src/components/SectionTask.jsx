b0185c2652bca7710dec92b87e7807d1fcae7982
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-12 04:19:33 +0200

    aggiunto input text e modificato con css

--- src/components/SectionTask.jsx
+++ src/components/SectionTask.jsx
@@ -4,8 +4,12 @@ function SectionTask(){
     return ( <>
         <div className="section-task-layout">
             <ul className="task-ul">
-                <li className="task-li"></li>
-                <li className="task-li"></li>
+                <li className="task-li">
+                    <input type="text" className="task-title" placeholder="Add a section task name..."/>
+                </li>
+                <li className="task-li">
+                    <input type="text" className="task-title" placeholder="Add a section task name..."/>
+                </li>
             </ul>
         </div>
         </>
