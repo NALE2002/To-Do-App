7785cab4c1576c061bae97aefca3b2816d955ab2
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-12 02:59:52 +0200

    aggiunto content nel main con grid column e row

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,8 +3,14 @@
 function MainContent(){
     return ( <>
         <div className="main-content">
-            
+            <div className="grid">
+                <div className="main-title">
+                    <h2>To-Do WebApp</h2>
+                </div>
+                <div className="section-task">
 
+                </div>
+            </div>
         </div>
 
         </>
