07c612daa36b956a8319e72ac7f04feba1ee2de7
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-14 18:30:35 +0200

    cambiamento grafico con test-2

--- src/components/SectionTask.jsx
+++ src/components/SectionTask.jsx
@@ -4,12 +4,12 @@ function SectionTask(){
     return ( <>
         <div className="section-task-layout">
             <ul className="task-ul">
-                <li className="task-li">
+                <li className="add-task-li">
                     <input type="text" className="task-title" placeholder="Add a section task name..."/>
                 </li>
-                <li className="task-li">
-                    <input type="text" className="task-title" placeholder="Add a section task name..."/>
-                </li>
+                {/* <li className="task-li">
+                    
+                </li> */}
             </ul>
         </div>
         </>
