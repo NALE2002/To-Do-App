d6b2230ac1df693268496fba508941b5f5eba2c1
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-14 19:32:28 +0200

    ulteriori cambiamenti

--- src/components/SectionTask.jsx
+++ src/components/SectionTask.jsx
@@ -4,12 +4,12 @@ function SectionTask(){
     return ( <>
         <div className="section-task-layout">
             <ul className="task-ul">
-                <li className="add-task-li">
+                <li className="task-li">
                     <input type="text" className="task-title" placeholder="Add a section task name..."/>
-                    <IoIosAdd size="2.7em" color="gray" className="add-icon-li"/>
+                    <IoIosAdd size="2.7em" className="add-icon-li"/>
                 </li>
                 <li className="task-li">
-                    
+                    <p>Annodamacrop enacoid fortnite sucking</p>
                 </li>
             </ul>
         </div>
