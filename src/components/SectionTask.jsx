2391419c66a79cb178e4fa599235d752824c142c
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-14 18:53:44 +0200

    ulteriori aggiornamenti sul main content

--- src/components/SectionTask.jsx
+++ src/components/SectionTask.jsx
@@ -1,4 +1,4 @@
-
+import { IoIosAdd } from "react-icons/io";
 
 function SectionTask(){
     return ( <>
@@ -6,10 +6,11 @@ function SectionTask(){
             <ul className="task-ul">
                 <li className="add-task-li">
                     <input type="text" className="task-title" placeholder="Add a section task name..."/>
+                    <IoIosAdd size="2.7em" color="gray" className="add-icon-li"/>
                 </li>
-                {/* <li className="task-li">
+                <li className="task-li">
                     
-                </li> */}
+                </li>
             </ul>
         </div>
         </>
