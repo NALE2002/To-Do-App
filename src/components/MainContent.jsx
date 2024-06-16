12ae583f541a39497308351cb5285860f368b25d
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-06-16 19:21:21 +0200

    rimosso grid in main content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,12 +3,10 @@ import SectionTask from "./SectionTask";
 function MainContent(){
     return ( <>
         <div className="main-content">
-            <div className="grid">
-                <div className="main-title">
-                    <h2>To-Do WebApp</h2>
-                </div>
-                <SectionTask/>
+            <div className="main-title">
+                <h2>To-Do WebApp</h2>
             </div>
+            <SectionTask/>
         </div>
 
         </>
